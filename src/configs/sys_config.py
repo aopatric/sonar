d151368dd381d0b6b166ca74c8e4e41943fe186c
aopatric <aopatric@mit.edu>
2024-11-01 18:35:13 -0400

    restore sys_config file

--- src/configs/sys_config.py
+++ src/configs/sys_config.py
@@ -3,7 +3,6 @@
 # is to simulate different real-world scenarios without changing the algorithm configuration.
 from typing import Dict, List, Literal, Optional
 import random
-import os
 from utils.types import ConfigType
 
 # from utils.config_utils import get_sliding_window_support, get_device_ids
@@ -159,7 +158,6 @@ CIFAR10_DSET = "cifar10"
 CIAR10_DPATH = "./datasets/imgs/cifar10/"
 
 NUM_COLLABORATORS = 1
-
 DUMP_DIR = "/mas/camera/Experiments/SONAR/abhi"
 
 mpi_system_config: ConfigType = {
